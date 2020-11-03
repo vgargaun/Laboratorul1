925563f7be92c1bdc6e29df4a67c909c15f1a58c
Veaceslav Gargaun <v.gargaun@unifun.com>
2020-11-03 17:06:16 +0200

    first commit

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,8 @@
+public class MainClass {
+    public static void main(String[] args) {
+
+        for(double i = 1; i <= 10; i++)
+            System.out.println(Rezultat.Analitic(i/10D));
+
+    }
+}
---
+++ src/main/java/Rezultat.java
@@ -0,0 +1,5 @@
+public class Rezultat {
+    public static double Analitic(double h){
+        return Math.pow(h,3)-h+1;
+    }
+}
