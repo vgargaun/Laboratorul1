070d0d9cdd9a7da3dbe498b0edab66d5e8ec927f
Veaceslav Gargaun <v.gargaun@unifun.com>
2020-11-04 16:50:58 +0200

    asdfghjikolp

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,8 +1,12 @@
 public class MainClass {
     public static void main(String[] args) {
+        double h1 = 0.1;
+        double h2 = 0.2;
 
-        for(double i = 1; i <= 10; i++)
-            System.out.println(Rezultat.Analitic(i/10D));
+//        for(double i = 0; i <= 10; i++)
+//            System.out.println(Rezultat.analitic(i/10D));
 
+        for(int i = 1; i <= 5; i++ )
+            System.out.println(Rezultat.eulre(h2));
     }
 }
--- src/main/java/Rezultat.java
+++ src/main/java/Rezultat.java
@@ -1,5 +1,20 @@
 public class Rezultat {
-    public static double Analitic(double h){
-        return Math.pow(h,3)-h+1;
+    private static double x = 0;
+    private static double u = 1;
+
+    public static double analitic(double h) {
+        return Math.pow(h, 3) - h + 1;
+    }
+
+    public static double eulre(double h) {
+
+        u = u + h * (3*Math.pow(x,2)-1);
+        x = x + h;
+        return u;
+    }
+
+    public static double eulerModificat(double h){
+
+        return 0;
     }
 }
