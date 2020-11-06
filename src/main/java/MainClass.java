0d21fab25b13a2ef23215d75571ce043311045a9
Veaceslav Gargaun <v.gargaun@unifun.com>
2020-11-06 14:41:05 +0200

    add euler modificat

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -3,10 +3,18 @@ public class MainClass {
         double h1 = 0.1;
         double h2 = 0.2;
 
-//        for(double i = 0; i <= 10; i++)
-//            System.out.println(Rezultat.analitic(i/10D));
+        System.out.println("Rezolvare prin metoda analitica");
+        for(double i = 0; i <= 10; i++)
+            System.out.println(Rezultat.analitic(i/10D));
 
-        for(int i = 0; i <= 5; i++ )
-            System.out.println(Rezultat.eulre(h2));
+        System.out.println("Rezolvare prin metoda Euler");
+        for(int i = 0; i < 11; i++ )
+            System.out.println(Rezultat.eulre(h1));
+
+        System.out.println("Rezolvare prin metoda Euler Modificata");
+        for(double i = 0; i < 10; i++)
+            System.out.println(Rezultat.eulerModificat(h1));
+
+
     }
 }
--- src/main/java/Rezultat.java
+++ src/main/java/Rezultat.java
@@ -1,20 +1,29 @@
 public class Rezultat {
     private static double x = 0;
     private static double u = 1;
+    private static double uModif = 1;
+    private static double xModif = 0;
 
     public static double analitic(double h) {
-        return -4*Math.pow(h, 3) + 3*Math.pow(h,2) + 1;
+        return -4 * Math.pow(h, 3) + 3 * Math.pow(h, 2) + 1;
     }
 
     public static double eulre(double h) {
 
-        u = u + h * (-12*Math.pow(x,2)+6*x);
+        u = u + h * mathFun(x);
         x = x + h;
         return u;
     }
 
-    public static double eulerModificat(double h){
+    public static double eulerModificat(double h) {
 
-        return 0;
+        uModif = uModif + (h / 2) * (mathFun(xModif) + mathFun(xModif + h) );
+        xModif = xModif + h;
+        return uModif;
+
+    }
+
+    private static double mathFun(double x) {
+        return -12 * Math.pow(x, 2) + 6 * x;
     }
 }
